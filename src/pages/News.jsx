29cf03e754e7a01af391f7aff5268b299ae2bf3f
AxelSeara <axelsearagomez@gmail.com>
2024-09-28 11:43:58 +0200

    again

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -28,7 +28,7 @@ const NewsPage = () => {
   return (
     <div>
       <section className="container mx-auto px-4 py-6">
-        <h2 className="text-3xl font-bold mb-4">Latest News</h2>
+        <h2 className="text-3xl font-bold mb-4 mt-16">Latest News</h2>
         <input
           type="date"
           value={filterDate}
