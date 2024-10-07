6fb6a07dc6b8dae3b44c5848c34febb80fbe42c4
AxelSeara <axelsearagomez@gmail.com>
2024-10-07 16:07:40 +0200

    under construction 3rd try

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,15 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Verifica si estamos en modo "Under Construction"
+const isUnderConstruction = import.meta.env.VITE_UNDER_CONSTRUCTION === 'true';
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  isUnderConstruction ? (
+    <h1 style={{ textAlign: 'center', marginTop: '20%' }}>Site Under Construction</h1>
+  ) : (
+    <StrictMode>
+      <App />
+    </StrictMode>
+  )
+);
